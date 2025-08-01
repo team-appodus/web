35e60b890c63e6a6c7d76594c5c8e6225cc1a5aa
kingsley.ezenwere <kingsley.ezenwere@longbridgetech.com>
2025-08-01 02:37:51 +0100

    add backend api to the .env file

--- src/containers/index.ts
+++ src/containers/index.ts
@@ -1,6 +1,7 @@
 import { ServerStore } from "@stores/server-store";import { AxiosHttpClient, HttpClient } from "@lib/http-client";
+import { config } from "@lib/config";
 
-const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL!;
+const baseURL = config.apiUrl;
 export const httpClient: HttpClient = new AxiosHttpClient(baseURL)
 
 export const serverStore: ServerStore = new ServerStore()
